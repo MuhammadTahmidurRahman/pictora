044f5b9038db9850d9aec0f57de17d611735c932
Muhammad Tahmidur Rahman <muhammad.rahman12@northsouth.edu>
2024-11-08 19:44:37 +0600

    ada

--- join_event.js
+++ join_event.js
@@ -1,4 +1,27 @@
 // Function to join room as a guest
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, fetchSignInMethodsForEmail } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
+import { getDatabase, ref as dbRef, set, get, query, orderByChild, equalTo } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHLMbTbLBS0mhw2dLFkLt4OzBEWyubr3c",
+  authDomain: "pictora-7f0ad.firebaseapp.com",
+  projectId: "pictora-7f0ad",
+  storageBucket: "pictora-7f0ad.appspot.com",
+  messagingSenderId: "155732133141",
+  databaseURL: "https://pictora-7f0ad-default-rtdb.asia-southeast1.firebasedatabase.app",
+  appId: "1:155732133141:web:c5646717494a496a6dd51c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const storage = getStorage();
+const database = getDatabase();
+
+
 async function joinRoomAsGuest(eventCode) {
     if (!user) {
       alert('Please log in to join the room!');
