5e50be13e9b4e4d529d0aafdd33dd5a257fb3b22
Muhammad Tahmidur Rahman <muhammad.rahman12@northsouth.edu>
2024-12-09 12:31:05 +0600

    firebase connection fixed

--- arrangedphoto.js
+++ arrangedphoto.js
@@ -17,13 +17,13 @@ import {
 
 // Firebase Initialization
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  databaseURL: "YOUR_DATABASE_URL",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDHLMbTbLBS0mhw2dLFkLt4OzBEWyubr3c",
+  authDomain: "pictora-7f0ad.firebaseapp.com",
+  projectId: "pictora-7f0ad",
+  storageBucket: "pictora-7f0ad.appspot.com",
+  messagingSenderId: "155732133141",
+  databaseURL: "https://pictora-7f0ad-default-rtdb.asia-southeast1.firebasedatabase.app",
+  appId: "1:155732133141:web:c5646717494a496a6dd51c",
 };
 
 const app = initializeApp(firebaseConfig);
